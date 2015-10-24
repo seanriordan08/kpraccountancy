bdadb48ec4bedd353ad8e7195c01fd73ca950ec4
seanriordan08 <sean.riordan08@gmail.com>
2015-10-24 15:19:12 -0600

    Add typed into jumbotron

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require typed.js-master/js/typed.js
 //= require_tree .
---
+++ app/assets/javascripts/welcome.js
@@ -0,0 +1,8 @@
+
+//Typed.js
+$(function(){
+  $(".active-typing").typed({
+    strings: ["First sentence.", "Second sentence."],
+    typeSpeed: 5
+  });
+});

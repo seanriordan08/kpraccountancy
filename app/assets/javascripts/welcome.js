d6d1f985507a2b7ed6bb096bc2520d8b6cac52f9
seanriordan08 <sean.riordan08@gmail.com>
2015-10-25 15:33:21 -0600

    Added transitions

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,12 +1,33 @@
 
 $(document).ready(function(){
 
-  $(".navbar-nav a.about").click(function(){
-    $('.view-port').addClass("fade");
-    $('.home').css("display", "none");
-    $('.about').css("display", "block");
-    $('.view-port').addClass("fade in");
+  $(".navbar-brand").on("click", function(){
+    show_welcome_home();
   });
+
+  $(".navbar-nav a.about").on("click", function(){
+    show_welcome_about();
+  });
+
+  function show_welcome_home(){
+    $('.view-port, .footer').fadeOut(250, function(){
+      setTimeout(function(){
+        $('.view-port .home').css("display", "block");
+        $('.view-port .about').css("display", "none");
+        $('.view-port, .footer').fadeIn(200);
+      }, 200);
+    });
+  }
+
+  function show_welcome_about(){
+    $('.view-port, .footer').fadeOut(250, function(){
+      setTimeout(function(){
+        $('.view-port .about').css("display", "block");
+        $('.view-port .home').css("display", "none");
+        $('.view-port, .footer').fadeIn(200);
+      }, 200);
+    });
+  }
 
 });
 

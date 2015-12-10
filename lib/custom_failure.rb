d1f91731ddad2e91d779fa77f34854100f249a9f
seanriordan08 <sean.riordan08@gmail.com>
2015-12-10 00:25:11 -0700

    Add redirect upon auth fail

---
+++ lib/custom_failure.rb
@@ -0,0 +1,17 @@
+class CustomFailure < Devise::FailureApp
+
+  def redirect_url
+    root_path
+  end
+
+  # You need to override respond to eliminate recall
+  def respond
+    if http_auth?
+      http_auth
+    else
+      flash[:error] = "Unable to authenticate!"
+      redirect
+    end
+  end
+
+end

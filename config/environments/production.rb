745fe20e4e06b88bc0a1e387d8dde1b99c5182f1
seanriordan08 <sean.riordan08@gmail.com>
2015-12-11 11:13:07 -0700

    Add mailer configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: '107.170.253.191', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'kpraccountancy.com'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).

4e02e11b9d01f98b9112adaa2ca93b469bc96866
seanriordan08 <sean.riordan08@gmail.com>
2015-12-11 14:00:32 -0700

    Remove account for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable, :timeoutable
 
-  belongs_to :account
-
   validates :first_name, presence: true
   validates :last_name, presence: true
 

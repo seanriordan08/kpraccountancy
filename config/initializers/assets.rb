674515d0499cc0ef5866a16534b008957d28ca12
seanriordan08 <sean.riordan08@gmail.com>
2015-12-16 20:34:57 -0700

    Disable production asset digest

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,9 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Disable fingerprinting till we're up on AWS S3
+config.assets.digest = false
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 # Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
